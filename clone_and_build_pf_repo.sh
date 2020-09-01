93cceb9910f767278d780b53c09c99c0beaf79b8
Ahmad Rezaii <ahmad.rezaii@gmail.com>
2020-08-31 20:20:34 -0700

    add usage on wrong args

--- clone_and_build_pf_repo.sh
+++ clone_and_build_pf_repo.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: clone_and_build_parflow <directory> <repository> <branch>"
+    exit 2
+fi
+
+
 DIR=$1
 REPO=$2
 BRANCH=$3
+
+
 
 ./clone_pf_repo.sh $REPO $BRANCH $DIR
 
