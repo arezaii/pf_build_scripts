ec7e81ed2652db8b6f59767362ce11ff8547db4a
Ahmad Rezaii <ahmadrezaii@u.boisestate.edu>
2020-11-09 09:57:07 -0700

    fix typo

--- rebuild_parflow.sh
+++ rebuild_parflow.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
-if [ "$#" -ne 2]; then
+if [ "$#" -ne 2 ]; then
     echo "Usage: rebuild_parflow <pf_libs directory> <ParFlow_directory>"
     exit 2
 fi
 
-# LIBDIR=$1
-# PFDIR=$2
-
-# cd $PFDIR
-# rm -rf install build
-
-# mkdir build install
-# cd build
-
-
-# cmake ../parflow -DCMAKE_INSTALL_PREFIX=${DIR}/install -DPARFLOW_AMPS_LAYER=mpi1 -DPARFLOW_ENABLE_TIMING=true -DPARFLOW_HAVE_CLM=ON -DTK_INCLUDE_PATH=${TK_INCLUDE_PATH} -DHYPRE_ROOT=${PARFLOW_HYPRE} -DSILO_ROOT=${PARFLOW_SILO} -DHDF5_ROOT=${PARFLOW_HDF5} -DTCL_INCLUDE_PATH=${TCL_INCLUDE_PATH} -DPARFLOW_AMPS_SEQUENTIAL_IO=True
-
-# make
-# make install
-
-# cd ../parflow/test/washita/tcl_scripts/
-# tclsh Dist_Forcings.tcl
-    # if [ "$#" -ne 2 ]; then
- #        echo "Usage: build_parflow <pf_libs directory> <parflow_dir>"
- #        exit 2
- #    fi
-
 PFLIBS=$1
 PFDIR=$2
-
 
 if [ ! -d $PFDIR ]; then    
  mkdir -p $PFDIR
@@ -64,6 +41,6 @@ cmake3 ../parflow \
 -DPARFLOW_ENABLE_TIMING=TRUE \
 -DPARFLOW_HAVE_CLM=ON \
 -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_INSTALL_PREFIX=${PARFLOW_DIR}/install && \
+-DCMAKE_INSTALL_PREFIX=${PFDIR}/install && \
 make install -j${CORE_COUNT} && \
 cd ..  
